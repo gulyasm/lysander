75b3f1f4b2acfd142e8652825742cea8683aa365
gulyasm <mgulyas86@gmail.com>
2013-08-07 13:15:05 +0200

    Payload can be added to the events.

--- test/src/hu/dmlab/lysander/collector/TestEvent.java
+++ test/src/hu/dmlab/lysander/collector/TestEvent.java
@@ -7,9 +7,11 @@ public class TestEvent extends Event {
 	public static String id = "3242423";
 	public static long timestamp = 1234;
 	public static String type = "mate";
+	public static int payload1 = 3;
+	public static int payload2 = 43;
 
 	public TestEvent() {
-		super(id, timestamp, type);
+		super(id, timestamp, type, payload1, payload2);
 	}
 
 }
